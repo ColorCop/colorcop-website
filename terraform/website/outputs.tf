178a4558f54e58b81f3368dad90f41cd4f4e1dca
Jay Prall <36337+j4y@users.noreply.github.com>
2024-03-21 19:38:39 -0400

    fix: automate invalidating the Cloudfront distribution
    Small tweak to the deploy workflow

--- terraform/website/outputs.tf
+++ terraform/website/outputs.tf
@@ -1,10 +1,4 @@
-output "certificate_arn" {
-  value = aws_acm_certificate_validation.cert_validation.certificate_arn
-}
-output "domain_name" {
-  value = aws_s3_bucket_website_configuration.bucket.website_endpoint
-}
-
-output "origin_id" {
-  value = aws_s3_bucket.bucket.bucket_regional_domain_name
+output "distribution_id" {
+  value = aws_cloudfront_distribution.distribution.id
+  description = "Cloudfront distribution ID.  This is used for invalidation"
 }
