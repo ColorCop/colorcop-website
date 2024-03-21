282edf9989543b41a3e9b03857045a82829f52fd
Jay Prall <36337+j4y@users.noreply.github.com>
2024-03-21 00:01:23 -0400

    fix: terraform example variable mistake

--- terraform/website/main.tf
+++ terraform/website/main.tf
@@ -3,14 +3,14 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
