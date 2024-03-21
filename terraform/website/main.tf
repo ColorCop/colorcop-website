49f07e92cf1d2a2e57174509f5dffcdce8539973
Jay Prall <36337+j4y@users.noreply.github.com>
2024-03-21 00:09:19 -0400

    fix: add s3 website bucket terraform configuration

--- terraform/website/main.tf
+++ terraform/website/main.tf
@@ -27,3 +27,16 @@ resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_website_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
