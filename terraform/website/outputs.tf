636e34f51821e48e5d1adb7ffa192559eff3ad8e
Jay Prall <36337+j4y@users.noreply.github.com>
2024-03-24 23:56:39 -0400

    chore: use terraform output to get the s3 bucket name

--- terraform/website/outputs.tf
+++ terraform/website/outputs.tf
@@ -2,3 +2,8 @@ output "distribution_id" {
   value = aws_cloudfront_distribution.distribution.id
   description = "Cloudfront distribution ID.  This is used for invalidation"
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.bucket.id
+  description = "Name of the S3 bucket that hosts the files for the website."
+}
