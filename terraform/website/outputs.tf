9beba3aba1b91e4b604c16fb82a380ae49a8df23
Jay Prall <36337+j4y@users.noreply.github.com>
2024-03-30 10:53:33 -0400

    chore: configure pre-commit to run linting and formatting on terraform (#25)
    To run manually:
    
        pre-commit run -a

--- terraform/website/outputs.tf
+++ terraform/website/outputs.tf
@@ -1,9 +1,9 @@
 output "distribution_id" {
-  value = aws_cloudfront_distribution.distribution.id
+  value       = aws_cloudfront_distribution.distribution.id
   description = "Cloudfront distribution ID.  This is used for invalidation"
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.bucket.id
   description = "Name of the S3 bucket that hosts the files for the website."
 }
